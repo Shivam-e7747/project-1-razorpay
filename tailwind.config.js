67e8e6e97af7cd6e62d0b21b8f0e80a49a57ffbc
Shivam-e7747 <shivamtiwar2006@gmail.com>
2024-10-26 19:22:32 +0530

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,33 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["*"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["*"],
+  theme: {
+    extend: {
+      fontFamily:{
+        mullish: ["Mulish","sans-serif"],
+      },
+      colors:{
+        deepBlue:"#02042a",
+        lightBlue:"#2b84ed",
+        lightBlue300:"#4b94ed",
+        lightBlue500:"#0b72e7",
+        greenLight:"#61cea6",
+        lightGray:"#e2e2e2",
+        grayBlue:"#344a6c",
+        deepBlueHead:"#162g56",
+        gray2:"#525a76",
+      },  
+    },
+
+  },
+  plugins: [],
+}
